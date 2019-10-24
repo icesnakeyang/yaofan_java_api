74c262ca972624921732d4423f3e5ce39f9660f3
snake <flow_ocean@yahoo.com>
2019-10-24 22:28:53 +0800

    progress

--- src/test/java/com/gogoyang/yaofan/YaofanApplicationTests.java
+++ src/test/java/com/gogoyang/yaofan/YaofanApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LifeCapsuleApplicationTests {
+class YaofanApplicationTests {
 
     @Test
     void contextLoads() {
